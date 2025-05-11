bbb63386bbe4845a7d008f0f279f07f6f1d5d74e
Eduardo Ruiz <dev@eruiz.es>
2025-05-11 11:05:22 +0000

    entrypoint end of file format

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,17 +5,26 @@
 #   sleep 2
 # done
 git config --global --add safe.directory /App
+
 echo "Database is ready. Executing 'composer install'..."
+
 composer install
+
 #echo "Setting up Git hooks..."
 #chmod +x setup-githook.sh
 #sh setup-githook.sh
+
 if [ ! -f .git/hooks/pre-commit ]
 then
   cp .env.example .env
 fi
+
 echo "Generating migrations..."
+
 php artisan migrate:refresh --seed
+
 echo "Starting Laravel application..."
+
 php artisan serve --host=0.0.0.0 --port=8000 
+
 sleep infinity
